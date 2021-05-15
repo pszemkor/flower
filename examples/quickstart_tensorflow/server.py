d326ff38151f00a61766f63401bf52215eab036d
Przemysław Jabłecki <37248877+pszemkor@users.noreply.github.com>
2021-05-15 15:51:15 +0200

    Update server.py

--- examples/quickstart_tensorflow/server.py
+++ examples/quickstart_tensorflow/server.py
@@ -17,8 +17,8 @@ class SaveModelStrategy(fl.server.strategy.FedAvg):
 # Start Flower server for three rounds of federated learning
 if __name__ == "__main__":
     strategy = SaveModelStrategy(
-        fraction_fit=0.5,  # Sample 10% of available clients for the next round
-        min_fit_clients=3,  # Minimum number of clients to be sampled for the next round
-        min_available_clients=6)  # Minimum number of clients that need to be connected to the server before a training round can start)
+        fraction_fit=1.0,  # Sample 10% of available clients for the next round
+        min_fit_clients=2,  # Minimum number of clients to be sampled for the next round
+        min_available_clients=2)  # Minimum number of clients that need to be connected to the server before a training round can start)
     
     fl.server.start_server("0.0.0.0:8080", config={"num_rounds": 5}, strategy=strategy)
